670ac62216b17ea14291391d18754c99e59fdbc6
elena-ignaev <ngkhanhvy11@gmail.com>
2023-06-01 18:17:53 +0800

    Create control.js

---
+++ static/js/control.js
@@ -0,0 +1,7 @@
+//pic1, pic2, pic3 - pictures
+function search() {
+    /*
+    take the html file you want to redirect to as the parameter: window.location.replace() */
+    var request = document.getElementById("seach-textbox").value;
+    // in the form of ?query=whatever window.location.replace();
+}
