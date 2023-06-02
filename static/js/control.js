6a1158aeb28010f6c1c2a950c1380b07abf6452e
elena-ignaev <ngkhanhvy11@gmail.com>
2023-06-02 12:37:52 +0800

    Update control.js

--- static/js/control.js
+++ static/js/control.js
@@ -1,8 +1,9 @@
-//pic1, pic2, pic3 - pictures
+const user_input = document.getElementById('search-textbox').value;
 function search() {
-    /*
-    take the html file you want to redirect to as the parameter: window.location.replace() */
-    var request = document.getElementById("seach-textbox").value;
-    // in the form of ?query=whatever window.location.replace();
+    // in the form of ?query=whatever window.location.replace(window.location.href);
+    // redirect by changing link address of webpage
+    // somehow receive data from python
+    // edit html file
+    // elements: search-textbox, search-button, pic1, pic2, pic3, loc1, loc2, loc3, des1, des2, des3
 }
 
